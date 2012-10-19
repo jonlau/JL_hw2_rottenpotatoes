0cd9521f6fdf152744b5fd37c3c615bdeffec64f
Jon Lau <jon.cw.lau@gmail.com>
2012-10-18 21:57:54 -0700

    Made changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,10 +4,22 @@ class MoviesController < ApplicationController
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
+
   end
 
   def index
-    @movies = Movie.all
+    sort = params[:sort_by]
+        #debugger
+    if sort == "name" then 
+      @movies = Movie.all(:order => 'title')
+      @title_style = 'hilite'
+    elsif sort=="date" 
+      @movies = Movie.all(:order => 'release_date asc')
+      @rdate_style = 'hilite'
+    else
+      @movies = Movie.all
+    end
+
   end
 
   def new
